5a3317edcfc0a96b5f77ab84ec5136b9b4e4272c
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-04-29 09:55:50 -0700

    style: add invalid listener pseudocode
    Also added standard file organization comments for clarity.

---
+++ script.js
@@ -0,0 +1,20 @@
+// ----------------------- Sign-Up Form JavaScript -----------------------------
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Pseudocode ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+- When user focuses on an input control, make CSS apply invalid analysis, as 
+  opposed to when the page loads
+    - User focuses on any input control
+    - Apply 'required' attribute. CSS should have input['required'] styles
+      already defined
+*/
+
+"use strict";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Execution ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Testing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
