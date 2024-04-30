5bdf677e04670fda09988904e4f6ddebca704b1e
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-04-30 16:07:08 -0700

    docs: add pseudocode for pswrd rqmt execution

--- script.js
+++ script.js
@@ -7,6 +7,15 @@
     - User focuses on any input control
     - Apply 'required' attribute. CSS should have input['required'] styles
       already defined
+
+- When user begins to input into password fields:
+   - Add the 3 or 1 divs with rqmt text. Add the red x content
+   - Apply .fail and remove .success
+   - Check if their input matches the 3 or 1 criteria (8 char, 1 capital, 1 num
+     or matching)
+   - If content doesn't match on any of the criteria, add the .fail class, and
+     remove .success class
+   - If content matches on any of the criteria, add .success and remove .fail
 */
 
 "use strict";
@@ -25,3 +34,4 @@ signupDiv.addEventListener('focusin', (e) => {
         e.target.setAttribute('required', 'required');
     };
 });
+
